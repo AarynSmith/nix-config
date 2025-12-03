c0241315b268125604d1130d5cec877b88c8180b
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2025-12-03 11:52:30 -0600

    cleanup and configure aerospace

--- home/darwin/aaryn.nix
+++ home/darwin/aaryn.nix
@@ -9,7 +9,6 @@
   home.packages = [
     pkgs.nixfmt-rfc-style
   ];
-
   programs.zsh.shellAliases = {
     nix-rebuild = "cd ~/.config/nix-config/; sudo darwin-rebuild switch --flake .";
     nix-registry = "sudo wget -O/etc/nix/flake-registry.json https://raw.githubusercontent.com/NixOS/flake-registry/master/flake-registry.json";
@@ -21,19 +20,6 @@
     gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
   };
 
-  desktop {
-    enableAreospace = true;
-    enableGhostty = true;
-  }
-
-  # xdg.configFile."ghostty/config".text = ''
-  #   theme = "Catppuccin Mocha"
-  #   background-blur-radius = 20
-  #   background-opacity = .85
-  #   unfocused-split-opacity = .75
-  #   clipboard-paste-protection = false
-  #   copy-on-select = true
-  # '';
-
-  # Mac-only packages / configs here
+  desktop.enableAerospace = true;
+  desktop.enableGhostty = true;
 }
--- home/modules/desktop/aerospace.nix
+++ home/modules/desktop/aerospace.nix
@@ -13,22 +13,49 @@ in
       enable = true;
 
       # start at login via launchd (Home Manager-managed)
-      launchd.enable = true;
+      launchd.enable = false;
 
       # TOML-ish config via userSettings
       userSettings = {
-        gaps.outer = {
-          left = 8;
-          right = 8;
-          top = 8;
-          bottom = 8;
+        after-startup-command = [ ];
+        start-at-login = false;
+        enable-normalization-flatten-containers = true;
+        enable-normalization-opposite-orientation-for-nested-containers = true;
+        accordion-padding = 30;
+        default-root-container-layout = "tiles";
+        default-root-container-orientation = "auto";
+        on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
+        automatically-unhide-macos-hidden-apps = false;
+        key-mapping = {
+          preset = "qwerty";
+        };
+
+        gaps = {
+          inner = {
+            horizontal = 8;
+            vertical = 8;
+          };
+          outer = {
+            left = 0;
+            right = 0;
+            top = 0;
+            bottom = 0;
+          };
         };
 
         "mode.main.binding" = {
+          "alt-slash" = "layout tiles horizontal veritcal";
+          "alt-comma" = "layout accordion horizontal vertical";
+
           "alt-h" = "focus left";
           "alt-j" = "focus down";
           "alt-k" = "focus up";
           "alt-l" = "focus right";
+
+          "alt-shift-h" = "move left";
+          "alt-shift-j" = "move down";
+          "alt-shift-k" = "move up";
+          "alt-shift-l" = "move right";
         };
       };
     };
