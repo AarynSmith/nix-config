9a182137c185e8c9dfab6c1ad9045ca8993c2c5c
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2025-12-03 14:24:11 -0600

    fix hammerspoon config

--- home/modules/desktop/hammerspoon.nix
+++ home/modules/desktop/hammerspoon.nix
@@ -9,16 +9,11 @@ let
 in
 {
   config = lib.mkMerge [
-    # Shared config file on all platforms
-    (lib.mkIf cfg.enableGhostty {
-      xdg.configFile."ghostty/config".text = ''
-        theme = "Catppuccin Mocha"
-        background-blur-radius = 20
-        background-opacity = .85
-        unfocused-split-opacity = .75
-        clipboard-paste-protection = false
-        copy-on-select = true
-      '';
+    (lib.mkIf cfg.enableHammerspoon {
+      home.file.".hammerspoon" = {
+        source = ../../../configurations/hammerspoon;
+        recursive = true;
+      };
     })
   ];
 }
