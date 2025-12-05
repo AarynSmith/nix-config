bf29e5443cefad5f4fc60cb1e3b1753097213f5c
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2025-12-05 12:00:37 -0600

    update configs

--- scripts/save-configs.sh
+++ scripts/save-configs.sh
@@ -1,1 +1,7 @@
-cp /Users/aaryn/Library/Preferences/com.jordanbaird.Ice.plist ./configurations/ice/com.jordanbaird.Ice.plist
+cp ~/Library/Preferences/com.jordanbaird.Ice.plist ./configurations/ice/com.jordanbaird.Ice.plist
+
+
+cp ~/Library/Application\ Support/VSCodium/User/keybindings.json ./configurations/vscodium/User/
+cp ~/Library/Application\ Support/VSCodium/User/settings.json ./configurations/vscodium/User/
+cp -r ~/Library/Application\ Support/VSCodium/User/snippets ./configurations/vscodium/User/
+codium --list-extensions > ./configurations/vscodium/extensions.txt
