c424808cbf300bba16c094cd93800be63b9141f1
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2025-12-16 22:13:05 -0600

    fix the unarchiver

--- modules/brewapps/the-unarchiver.nix
+++ modules/brewapps/the-unarchiver.nix
@@ -8,20 +8,7 @@ let
   cfg = config.brewapps;
 in
 {
-  config = lib.mkIf cfg.enableGhostty {
-    # Not currently available for darwin
-    # programs.ghostty = {
-    #   # enable = true;
-    #   enableZshIntegration = true;
-    #   installBatSyntax = true;
-    #   settings = {
-    #     theme = "Catppuccin Mocha";
-    #     background-blur-radius = 20;
-    #     background-opacity = .85;
-    #     unfocused-split-opacity = .75;
-    #     clipboard-paste-protection = false;
-    #     copy-on-select = true;
-    #   };
+  config = lib.mkIf cfg.enableTheUnarchiver {
     homebrew = {
       enable = true;
       casks = [
